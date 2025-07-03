9bd544d15923f648ac92e6adadd3cfcfaeb8b53d
Huwensong <huwensongsw@163.com>
2025-07-03 14:03:56 +0800

    add & test full font

--- gui/ui/RuntimeFont.cpp
+++ gui/ui/RuntimeFont.cpp
@@ -16,11 +16,14 @@ namespace UI
 
     bool RuntimeFont::_initialize()
     {
-        lv_font_t *font = lv_bin_runtime_create("S:..\\resources\\cbin_16.cbin");
+        lv_font_t *font = lv_bin_runtime_create("S:..\\resources\\lt_pro_normal.cbin");
         lv_obj_set_style_text_font(label_runtime,font,0);
         lv_obj_set_style_text_color(label_runtime,lv_color_white(),0);
         lv_obj_set_style_text_align(label_runtime,LV_TEXT_ALIGN_CENTER,0);
-        lv_label_set_text(label_runtime,"中文测试");
+        lv_label_set_text(label_runtime,"窗前明月光，\n"
+                                        "疑是地上霜，\n"
+                                        "举头望明月，\n"
+                                        "低头思故乡。\n");
         lv_obj_align(label_runtime,LV_ALIGN_TOP_MID,0,0);
 
         lv_obj_set_style_text_font(label_font,&cbin_16,0);
